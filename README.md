bcbd89d1d4f57d22e69c7394306da92b457a972c
unknown <csweeney98@gmail.com>
2019-03-27 12:34:20 -0400

    Initial commit :sparkle:

---
+++ README.md
@@ -0,0 +1,3 @@
+## Community Forum DB
+
+![](https://www.lucidchart.com/publicSegments/view/6e8dc976-dfc5-46f4-a78f-05330d79584d/image.png)
---
+++ knexfile.js
@@ -0,0 +1,10 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://forumadmin:pass123@localhost/communityforum',
+  },
+
+};
